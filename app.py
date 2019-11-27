9a17430b19703755c7dd941eb46b97b0fdf8fe77
unknown <alanpierre9298@gmail.com>
2019-11-27 15:08:44 +0100

    correcting the limiter

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    default_limits=["5 per minute", "1 per second"],
+    default_limits=["5 per minute", "1 per second"])
 
 @app.route('/')
 def home():
