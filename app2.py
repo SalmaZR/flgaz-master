cf0b15326e7fa246c06d99d35fb68c0c75d49ed0
Salma <salma.zraibi@gmail.com>
2019-11-27 16:58:22 +0100

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,124 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import csv
+
+app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["5 per minute", "1 per second"])
+
+@app.route('/')
+def home():
+    return 'Bienvenue !'
+
+@app.route('/gaz', methods=['GET','POST'])
+def save_gazouille():
+	if request.method == 'POST':
+		print(request.form)
+		dump_to_csv(request.form)
+		return redirect(url_for('timeline'))
+		#return "OK"
+	if request.method == 'GET':
+		return render_template('formulaire.html')
+
+@app.route('/timeline', methods=['GET'])
+def timeline():
+	gaz = parse_from_csv()
+	return render_template("timeline.html", gaz = gaz)
+
+def parse_from_csv():
+	gaz = []
+	with open('./gazouilles.csv', 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			gaz.append({"user":row[0], "text":row[1]})
+	return gaz
+
+def dump_to_csv(d):
+	donnees = [d["user-name"],d["user-text"] ]
+	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
+		writer = csv.writer(f)
+		writer.writerow(donnees)
+
+from flask import Flask, Response, redirect, url_for, request, session, abort
+from flask.login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user 
+
+# config
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+# create some users with ids 1 to 20       
+users = [User(id) for id in range(1, 21)]
+
+
+# some protected url
+@app.route('/')
+@login_required
+def home():
+    return Response("Hello World!")
+
+ 
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']        
+        if password == username + "_secret":
+            id = username.split('user')[1]
+            user = User(id)
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+    
+    
+# callback to reload the user object        
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
