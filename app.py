20f40065521c3bab124f1412af56a5f6ea8a50ba
Salma <salma.zraibi@gmail.com>
2019-11-28 12:09:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,17 @@ def home():
 
 @app.route('/gaz', methods=['GET','POST'])
 def save_gazouille():
-	if request.method == 'POST':
-		print(request.form)
-		dump_to_csv(request.form)
-		return redirect(url_for('timeline'))
+    if not session.get('logged_in'):
+	    return render_template('login.html')
+    else:
+
+        if request.method == 'POST':
+            print(request.form)
+            dump_to_csv(request.form)
+            return redirect(url_for('timeline'))
 		#return "OK"
-	if request.method == 'GET':
-		return render_template('formulaire.html')
+        if request.method == 'GET':
+            return render_template('formulaire.html')
 
 @app.route('/timeline', methods=['GET'])
 def timeline():
